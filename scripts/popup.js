44c7018301bdffcdbdc048bda5caedd1aecfd0f0
duckimann <facoder2k2@gmail.com>
2020-08-27 18:50:36 +0700

    incl. options in popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+chrome.storage.local.get("opts", ({opts: opts}) => {
+	console.log(opts);
+	for (let inpName in opts) if (inpName) document.querySelector(`[name='${inpName}'][type='checkbox']`).checked = opts[inpName];
+});
+
+document.body.onchange = (a) => {
+	let values = {};
+	for (let checkbox of Array.from(document.querySelectorAll("input[type='checkbox']"))) values[checkbox.name] = checkbox.checked;
+	chrome.storage.local.set({ opts: values });
+};
