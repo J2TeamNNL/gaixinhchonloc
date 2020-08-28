14f26fb3061376b55078dc645bd8bb4b508fd387
Long <j2teamnnl@gmail.com>
2020-08-28 08:38:58 +0700

    xử lý conflict

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,3 @@
 'use strict';
 
-chrome.storage.local.get("opts", ({opts: opts}) => {
-	console.log(opts);
-	for (let inpName in opts) if (inpName) document.querySelector(`[name='${inpName}'][type='checkbox']`).checked = opts[inpName];
-});
-
-document.body.onchange = (a) => {
-	let values = {};
-	for (let checkbox of Array.from(document.querySelectorAll("input[type='checkbox']"))) values[checkbox.name] = checkbox.checked;
-	chrome.storage.local.set({ opts: values });
-};
+console.log('\'Allo \'Allo! Popup');
