18d57b83404f125de2b6921c5c8da78bb39ca23a
Long <j2teamnnl@gmail.com>
2020-08-28 08:42:11 +0700

    xử lý nốt

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+chrome.storage.local.get("opts", function (_ref) {
+  var opts = _ref.opts;
+  console.log(opts);
+
+  for (var inpName in opts) {
+    if (inpName) document.querySelector("[name='".concat(inpName, "'][type='checkbox']")).checked = opts[inpName];
+  }
+});
+
+document.body.onchange = function (a) {
+  var values = {};
+
+  for (var _i = 0, _Array$from = Array.from(document.querySelectorAll("input[type='checkbox']")); _i < _Array$from.length; _i++) {
+    var checkbox = _Array$from[_i];
+    values[checkbox.name] = checkbox.checked;
+  }
+
+  chrome.storage.local.set({
+    opts: values
+  });
+};
