bed1c934a28d2dacfa1751afaf0d73d860c82fb0
duckimann <facoder2k2@gmail.com>
2020-09-01 14:05:59 +0700

    updateBadge & send msg to background

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,14 @@
 'use strict';
 
+// Functions
+function updateBadge(str = "String", color = "#155997") {
+  // Because the badge has limited space, it should have 4 characters or less.
+  chrome.browserAction.setBadgeBackgroundColor({ color: color });
+  chrome.browserAction.setBadgeText({ text: str });
+}
+
 (function (chrome) {
+  // Long-lived connection
   chrome.runtime.onConnect.addListener(function (port) {
     port.onMessage.addListener(function (msg) {
       fetch(msg.url).then(function (data) {
@@ -11,5 +19,16 @@
         });
       });
     });
+  });
+
+  // Short-lived connection. Actually just send the msg and quit.
+  chrome.runtime.onMessage.addListener((msg, from, response) => {
+    let fncTable = {
+      updateBadge: function() {
+        updateBadge(msg.updateBadge.text, msg.updateBadge.color);
+      }
+    };
+    
+    fncTable[Object.keys(msg)[0]]();
   });
 })(chrome);
