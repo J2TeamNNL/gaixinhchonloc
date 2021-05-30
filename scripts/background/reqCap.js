4952d85cb540899f4b656647d1797d0b61ef02a0
duckimann <facoder2k2@gmail.com>
2021-05-30 17:38:44 +0700

    Flickr: dl btn integrated && debounce

--- scripts/background/reqCap.js
+++ scripts/background/reqCap.js
@@ -1,10 +1,10 @@
 chrome.webRequest.onBeforeRequest.addListener((catches) => {
 	let urlTable = {
-		"https://t.flickr.com/com.snowplowanalytics.snowplow/tp2": () => {
-			chrome.tabs.executeScript(catches.tabId, {
-				file: "scripts/injects/flickr.js", // [Optional] || Path from the root of ext. Ex: Err: injectScript/script.js | Success: js/injectScript/script.js
-			});
-		}
+		// "https://t.flickr.com/com.snowplowanalytics.snowplow/tp2": () => {
+		// 	chrome.tabs.executeScript(catches.tabId, {
+		// 		file: "scripts/injects/flickr.js", // [Optional] || Path from the root of ext. Ex: Err: injectScript/script.js | Success: js/injectScript/script.js
+		// 	});
+		// }
 	}
 	if (catches.url in urlTable) urlTable[catches.url]();
 }, { urls: ["<all_urls>"], types: ["main_frame", "xmlhttprequest", "media"] });
--- scripts/injects/flickr.js
+++ scripts/injects/flickr.js
@@ -1,41 +1,30 @@
 function appendHTML() {
-	if (document.readyState !== "complete") return;
-	if (document.querySelector("#gxcl_flickr")) return;
-	if (!document.querySelector(".view.is-ready")) return;
-	let btn = new DOMParser().parseFromString(`
-		<div id="gxcl_flickr" style="display: none;background: rgba(255, 255, 255, .5);width: fit-content;border-radius: 1em;margin: .5em 0 0 .5em;">
-			<a href="#" style="color: #000;text-decoration: none;padding: .5em 1em;font-size: 1.2em;">Download</a>
-		</div>
-	`, "text/html").querySelector("div"),
-		view = document.querySelector(".view.is-ready");
-	btn.querySelector("a").onclick = (ev) => {
-		ev.preventDefault();
-		new Promise((resolve) => {
-			let sizesTable = ["o", "6k", "5k", "f", "4k", "3k", "k", "h", "b", "c", "z", "w", "n", "m", "t", "q", "s"],
-				photoId = (new URL(document.URL)).pathname.match(/[^\\\/]+/g)[2],
-				photoSizes = JSON.parse(document.querySelector(".modelExport").outerHTML.match(/(?<=modelExport:\s?)\{.*\}/g)).main["photo-models"][0].sizes,
-				largest = `https:${photoSizes[sizesTable.find((curr) => (curr in photoSizes))].url}`;
-			if (largest.includes(photoId)) {
-				resolve(largest);
-			} else {
-				fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=621b3655c517a25e35ec61f6b9e4fbf3&photo_id=${ photoId }&format=json&nojsoncallback=1`)
-					.then((res) => res.json())
-					.then((res) => res.sizes.size.slice(-1)[0].source)
-					.then((res) => resolve(res));
-			}
-		}).then((a) => {
-			chrome.runtime.sendMessage({ download: a });
-		});
-	};
-	view.onmouseover = (mouse) => {
-		document.querySelector("#gxcl_flickr").style.display = "block";
-	};
-	view.onmouseleave = (mouse) => {
-		document.querySelector("#gxcl_flickr").style.display = "none";
-	}
-	view.appendChild(btn);
-	console.log("injected.", view);
+	setTimeout(() => {
+		let btn = document.querySelector("a[title*='Download'] > i");
+		if (btn && btn.parentElement.style.background != "green" && ["interactive", "complete"].includes(document.readyState)) {
+			btn.parentElement.style.background = "green";
+			btn.onclick = (ev) => {
+				ev.preventDefault();
+				new Promise((resolve) => {
+					let sizesTable = ["o", "6k", "5k", "f", "4k", "3k", "k", "h", "b", "c", "z", "w", "n", "m", "t", "q", "s"],
+						photoId = (new URL(document.URL)).pathname.match(/[^\\\/]+/g)[2],
+						photoSizes = JSON.parse(document.querySelector(".modelExport").outerHTML.match(/(?<=modelExport:\s?)\{.*\}/g)).main["photo-models"][0].sizes,
+						largest = `https:${photoSizes[sizesTable.find((curr) => (curr in photoSizes))].url}`;
+					if (photoSizes && largest.includes(photoId)) {
+						resolve(largest);
+					} else {
+						fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=621b3655c517a25e35ec61f6b9e4fbf3&photo_id=${ photoId }&format=json&nojsoncallback=1`)
+							.then((res) => res.json())
+							.then((res) => res.sizes.size.slice(-1)[0].source)
+							.then((res) => resolve(res));
+					}
+				}).then((a) => {
+					chrome.runtime.sendMessage({ download: a });
+				});
+			};
+			console.log("Injected.");
+		}
+	}, 1000);
 }
-
 appendHTML();
 document.onreadystatechange = () => appendHTML();
